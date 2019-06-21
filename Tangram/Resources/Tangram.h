e2931c527e282388f04193f0f4f85a9e0f0f8b2d
Jordan Kay <jordan@cultivr.co>
2019-06-21 10:13:21 -0700

    WIP

---
+++ Tangram/Resources/Tangram.h
@@ -0,0 +1,19 @@
+//
+//  Tangram.h
+//  Tangram
+//
+//  Created by Jordan Kay on 2/22/18.
+//  Copyright © 2018 Jordan Kay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Tangram.
+FOUNDATION_EXPORT double TangramVersionNumber;
+
+//! Project version string for Tangram.
+FOUNDATION_EXPORT const unsigned char TangramVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tangram/PublicHeader.h>
+
+
